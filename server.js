d7f3e7c6daf78761dbe375f5342813c28ba98ca6
Grant Kyle <sgrantkyle@gmail.com>
2020-05-20 15:51:01 -0500

    server set up

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.use(express.static("public"));
 
 
 // Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+}); 
