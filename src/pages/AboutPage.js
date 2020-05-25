c74cf498bcff05f7e00bd20c7ee3e442081ecef3
Grant Kyle <sgrantkyle@gmail.com>
2020-05-24 20:25:35 -0500

    bio update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,11 +9,16 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-                My start as an SDR at HotSchedules led me to do sales for a digital marketing agency, where I decided to make the transition from sales to SEO account management. As an SEO, I have manipulated HTML & CSS and looked at JavaScript all day for years without actually understanding what the code is doing holistically; this realization is what organically led me to attend a full-stack software development bootcamp at the University of Texas. 
-                <br/>
-                <br/>
-                As my time in bootcamp quickly proceeds, I am learning in immense waves just how interconnected marketing and development truly are and how these roles could both be made more efficient if they interacted in a more interdependent manner.
-                My background in digital marketing, coupled with my rapidly evolving skillset as a developer makes me a valuable asset to any front-end or marketing team I join: I am conversion-focused and am able to implement a marketing strategy with development-friendly tactics in mind and vice versa, I can develop with marketing-friendly tactics in mind. I am currently on the job market and would love to speak to any recruiter or hiring management about how my skillset can be used as an asset.
+                <h2> My History</h2>
+                My start as an SDR at HotSchedules led me to do sales for a digital marketing agency, where I decided to make the transition from sales to SEO account management. As an SEO, I manipulated HTML & CSS and looked at JavaScript all day for years without actually understanding what the code was doing holistically; this realization organically led me to attend a full-stack development bootcamp at the University of Texas.
+                <br />
+                <br />
+                As a junior developer, I am learning every day just how interconnected marketing and development are and how these roles could both be made more efficient if they interacted in a more interdependent manner. I am conversion-focused and am able to implement a marketing strategy with development-friendly tactics in mind and vice versa, I can develop with marketing-friendly tactics in mind. 
+
+                <h2>A bunch of random things I love working with</h2>
+                React, Node, SCSS, Redux, SQL, MongoDB, Jira, Postman, Heroku, Netlify, VS Code, Github, SEMrush, Ahrefs, Analytics, Search Console... and countless other things!
+                
+
 
             </Content>
 
