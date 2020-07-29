b674ce4d26ff7912429f5108d95290246e959e3c
Grant Kyle <sgrantkyle@gmail.com>
2020-07-28 22:02:54 -0500

    changes to be more mobile friendly

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,10 @@ const Header = (props) => {
   return (
     <div className="row gkp-header">
       <Navbar bg="light" expand="lg">
-        <div className="col-sm-3 gkp-header-items gkp-header--projects">
+        <div className="col-sm-6 gkp-header-items gkp-header--projects">
           <Navbar.Brand className="gkp-header--text" href="/">Home</Navbar.Brand>
         </div>
-        <div className="col-sm-3 gkp-header-items gkp-header--about">
+        <div className="col-sm-6 gkp-header-items gkp-header--about">
           <Nav.Link className="gkp-header--text" href="/projects">Projects</Nav.Link>
         </div>
         {/* <div className="col-sm-3 gkp-header-items gkp-header--contact">
