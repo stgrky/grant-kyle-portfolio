6df3e693f6e58e91a1f362e32251311e57b87efc
Grant Kyle <sgrantkyle@gmail.com>
2020-08-19 10:38:26 -0500

    gif additions

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -21,10 +21,17 @@ const Projects = () => {
                   >
                     Jobless.com
                   </a>
-                  : Jobless is a job listing marketplace where candidates can search for job listings and submit blog posts relating to employment or jobs in general. Currently, we are in the process of making the UI more user-friendly and mobile-friendly, and improving our search results UX. The site is made with Node and Express with a MongoDB database hosted on Atlas. We use React on the frontend and it is deployed on Heroku.
+                  : Jobless is a job listing marketplace where candidates can
+                  search for job listings and submit blog posts relating to
+                  employment or jobs in general. Currently, we are in the
+                  process of making the UI more user-friendly and
+                  mobile-friendly, and improving our search results UX. The site
+                  is made with Node and Express with a MongoDB database hosted
+                  on Atlas. We use React on the frontend and it is deployed on
+                  Heroku.
                 </p>
               </li>
-              <li>
+              {/* <li>
                 <p>
                   <a
                     className="gkp-projects--content-container-link"
@@ -32,9 +39,12 @@ const Projects = () => {
                   >
                     COVID-19 Patient Database (Mock)
                   </a>
-                  : This is a a mock website where doctors can create a login and add patients to a COVID-19 database. The website is made with Node, Express and Express handlebars, using Vue on the frontend. It is deployed on Heroku. 
+                  : This is a a mock website where doctors can create a login
+                  and add patients to a COVID-19 database. The website is made
+                  with Node, Express and Express handlebars, using Vue on the
+                  frontend. It is deployed on Heroku.
                 </p>
-              </li>
+              </li> */}
               <li>
                 <p>
                   <a
@@ -43,10 +53,28 @@ const Projects = () => {
                   >
                     Github User Finder
                   </a>
-                  : This app lets users search for any github user by either username, name or by any query relating to a user's profile. This is a frontend app built with React. It uses Context API, Hooks and is written with functional components. It is deployed on Netlify.
+                  : This app lets users search for any github user by either
+                  username, name or by any query relating to a user's profile.
+                  This is a frontend app built with React. It uses Context API,
+                  Hooks and is written with functional components. It is
+                  deployed on Netlify.
                 </p>
               </li>
             </ul>
+          </div>
+        </section>
+        <section className="row gkp-projects--gifs">
+          <div className="row gkp-projects--gif-container">
+            <div className="col-lg-4 gkp-projects--social-links-gif">
+              <img
+                alt="social-links-gif"
+                className="gkp-projects--social-link-gif"
+                src="../assets/gifs/social-link-gif.gif"
+              />
+              <p className="gkp-projects--social-links-gif-text">
+                This social link tab uses RegEx to validate whether or not the link is a valid URL. The social links that are added are then conditionally rendered on a card in the company's profile page.
+              </p>
+            </div>
           </div>
         </section>
       </div>
