a2280482771a88dfa40ac64b094d6c9401c20c70
Grant Kyle <sgrantkyle@gmail.com>
2021-08-24 18:49:59 -0500

    new projects added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -17,8 +17,8 @@ const Projects = () => {
               >
                 React Web Developer at RehabPath
               </a>
-              : I currently love my role working on a smaller but growing, mission-driven team
-              on their{" "}
+              : I currently love my role working on a smaller but growing,
+              mission-driven team on their{" "}
               <a
                 rel="noopener noreferrer"
                 target="_blank"
@@ -30,7 +30,7 @@ const Projects = () => {
               our marketing and development team; essentially I carry out
               development tasks that facilitate our marketing success. Daily, I
               work with React, a headless Gatsby.js CMS, graphQL and a blog
-              hosted through Wordpress. 
+              hosted through Wordpress.
             </p>
           </li>
           <li>
@@ -45,20 +45,48 @@ const Projects = () => {
               </a>
               : Before RehabPath, I completed a one-year contract as a full
               stack developer on a small, highly-agile early-stage startup
-              software development team. I implement small to medium-sized
+              software development team. I implemented small to medium-sized
               experiences that are featured on Paperstreet's production-level
               web application, servicing thousands of users and hundreds of
-              companies. I primarily worked on the front end but am able to work
-              in the back end as needed to implement or utilize APIs for rich,
-              front end functionality. Below are a few features I have
-              completed.
+              companies. I primarily worked on the front end but worked on the
+              back end as needed to implement or utilize APIs for rich, front
+              end functionality.
             </p>
           </li>
         </ul>
       </div>
       <h1 className="gkp-projects__title">Projects</h1>
+      <h2 className="gkp-projects__sub-title">
+        In order of my best to my okay-est
+      </h2>
       <div className="gkp-projects__content-container">
         <ul>
+          <li>
+            <p>
+              <a
+                className="gkp-projects__content-container-link"
+                href="https://limitless-anchorage-13670.herokuapp.com/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Cryptocurrency Loan Calculator
+              </a>
+              : Application that lets its user put in a loan amount, the
+              loan-term, the loan-to-value (LTV) ratio, and to choose between
+              interest only payments or principal + interest payments. It then
+              displays the info in another panel and uses a crypto API from Coin
+              Gecko to get live data on the value of the loan in different
+              cryptocurrencies. Built strictly with React, SCSS and a CoinGecko
+              API. Candidly, if I had one proud project on this portfolio, it's
+              this one; check it out!
+              <br />
+              <b>
+                *** Please note that this project is deployed for free with
+                Heroku, and may load very slowly. Please refresh and try again
+                if it times out ***
+              </b>
+            </p>
+          </li>
           <li>
             <p>
               <a
@@ -86,17 +114,15 @@ const Projects = () => {
               >
                 Train Fare Calculator
               </a>
-              : Not really a "calculator", it is parsing through data in a local
-              JSON file and using React Hooks to take the user to the
-              appropriate ticket and multiplying it by the amount of tickets
-              they want to get their fare total. Its simple user-interface and
-              sound logic makes it a presentable application. I got a lot of
-              useEffect practice with this application. Built with React,
-              deployed to Heroku.{" "}
+              : Parses through JSON data to take the user to the appropriate
+              ticket and multiplies it by the amount of tickets they want to get
+              their fare total. Its simple user-interface and sound logic makes
+              it a presentable application. Built with React, deployed to
+              Heroku. <br />
               <b>
-                *** Please note that this project is deployed to Heroku, and may
-                load very slowly. Please refresh and try again if it times out
-                ***
+                *** Please note that this project is deployed for free with
+                Heroku, and may load very slowly. Please refresh and try again
+                if it times out ***
               </b>
             </p>
           </li>
@@ -110,15 +136,15 @@ const Projects = () => {
               >
                 Mock Landing Page
               </a>
-              : This is a mock landing page for a fake business. I chose to
-              display it here because of its mobile-friendliness: it looks great
-              on desktop, and is responsive on screens the size of iPhone 5 all
-              the way to iPad Pro. Built with React and SCSS, deployed to
-              Heroku.{" "}
+              : A mock landing page for a fake business. I chose to display it
+              here because of its mobile-friendliness: it looks great on
+              desktop, and is responsive on screens the size of iPhone 5 all the
+              way to iPad Pro. Built with React and SCSS, deployed to Heroku.{" "}
+              <br />
               <b>
-                *** Please note that this project is deployed to Heroku, and may
-                load very slowly. Please refresh and try again if it times out
-                ***
+                *** Please note that this project is deployed for free with
+                Heroku, and may load very slowly. Please refresh and try again
+                if it times out ***
               </b>
             </p>
           </li>
@@ -142,27 +168,6 @@ const Projects = () => {
               for service industry profesionals to rate customers they serve
               (like a reverse Yelp). Built with PHP, CSS and HTML, hosted on
               Wordpress through Gatorhost.
-            </p>
-          </li>
-          <li>
-            <p>
-              <a
-                className="gkp-projects__content-container-link"
-                href="https://paperstreet.vc/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Software Developer at Paperstreet.vc
-              </a>
-              : I recently completed a one-year contract as a full stack
-              developer on a small, highly-agile early-stage startup software
-              development team. I implement small to medium-sized experiences
-              that are featured on Paperstreet's production-level web
-              application, servicing thousands of users and hundreds of
-              companies. I primarily worked on the front end but am able to work
-              in the back end as needed to implement or utilize APIs for rich,
-              front end functionality. Below are a few features I have
-              completed.
             </p>
           </li>
         </ul>
